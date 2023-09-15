e450b7b43c913ec667e7334b5571a219c5cea7f6
Paulo Victor <paulo.dev1401@gmail.com>
2023-09-15 06:39:15 -0300

    product pagination

--- src/main/java/com/PauloVictor/dscommerce/services/ProductService.java
+++ src/main/java/com/PauloVictor/dscommerce/services/ProductService.java
@@ -3,6 +3,8 @@ package com.PauloVictor.dscommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,7 +25,12 @@ public class ProductService {
 		ProductDTO dto = new ProductDTO(product);
 		return dto;		
 	}
-
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
+	}
 		
 		
 		
