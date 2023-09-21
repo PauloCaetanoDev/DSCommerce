a10272c44edd676efe27053da31b7494b780b3f7
Paulo Victor <paulo.dev1401@gmail.com>
2023-09-21 19:57:31 -0300

    Custom validation errors

---
+++ src/main/java/com/PauloVictor/dscommerce/DTO/FieldMessage.java
@@ -0,0 +1,23 @@
+package com.PauloVictor.dscommerce.DTO;
+
+public class FieldMessage {
+
+	private String fieldName;
+	private String message;
+	
+	public FieldMessage(String fieldName, String message) {
+		super();
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+	
+}
---
+++ src/main/java/com/PauloVictor/dscommerce/DTO/ValidationError.java
@@ -0,0 +1,28 @@
+package com.PauloVictor.dscommerce.DTO;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError {
+
+	public ValidationError(Instant timestamp, Integer status, String error, String path) {
+		super(timestamp, status, error, path);
+	}
+
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+	
+	public void addError (String fieldName, String message) {
+		
+		errors.add(new FieldMessage(fieldName, message));	
+	}
+	
+	
+	
+	
+	
+}
